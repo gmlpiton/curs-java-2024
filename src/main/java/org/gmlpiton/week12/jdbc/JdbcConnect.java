8a00093418557a32d141de651b32f46781157cab
gmlpiton@gmail.com <gmlpiton@gmail.com>
2024-05-08 23:28:04 +0300

    week 12 attempt 2

--- src/main/java/org/gmlpiton/week12/jdbc/JdbcConnect.java
+++ src/main/java/org/gmlpiton/week12/jdbc/JdbcConnect.java
@@ -1,2 +1,37 @@
-package org.gmlpiton.week12.jdbc;public class JdbcConnect {
+package org.gmlpiton.week12.jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class JdbcConnect {
+
+    private JdbcConnect() {
+            }
+
+public static Connection getConnection() throws SQLException {
+    System.out.println("Getting connection");
+    String url = new StringBuilder()
+            .append("jdbc:")
+            .append("postgresql") // “mysql” / “db2” / “mssql” / “oracle” / ...
+            .append("://")
+            .append("localhost")
+            .append(":")
+            // In your case it will be 5432
+            //            .append("5432")
+            .append("5432")
+            .append("/")
+            .append("test")
+            .append("?user=")
+            .append("postgres")
+            .append("&password=")
+            .append("test").toString();
+
+    System.out.println("URL: " + url);
+    System.out.println();
+
+    return DriverManager.getConnection(url);
+
+}
+
+
 }
